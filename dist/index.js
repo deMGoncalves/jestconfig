8fe2154b8139f2282289af7099c249a2d603ab28
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-02-23 13:15:52 -0300

    build(dist): automatic new publish

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,7 @@ module.exports = {
         }
     },
     moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@/(.*)$': '<rootDir>/src/$1'
     },
     preset: 'ts-jest',
     testEnvironment: 'node',
